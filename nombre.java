092aa11076a5b28dc628f0fed0e4bec7b7d46da4
Jugador <dm.evilart@gmail.com>
2017-09-22 13:48:24 +0200

    Fin de clase

--- nombre.java
+++ nombre.java
@@ -1,6 +1,6 @@
 public class nombre{
  public static void main(String args[]){
-
+/** Hora de inicio 12:38, pausa 13:40.  */
 	System.out.println("Nombre:Daniel Carmenes Alvarez");
     System.out.println("Edad:25");
 	System.out.println("Localidad de Nacimiento: Leon");
