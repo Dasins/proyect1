739297f75c6144cb4f3e5fec12b56c7caadb296a
Jugador <dm.evilart@gmail.com>
2017-09-22 13:31:28 +0200

    Nombre, fecha, localidad y Septiembre

--- nombre.java
+++ nombre.java
@@ -4,6 +4,7 @@ public class nombre{
 	System.out.println("Nombre:Daniel Carmenes Alvarez");
     System.out.println("Edad:25");
 	System.out.println("Localidad de Nacimiento: Leon");
-	}
+	System.out.println("Septiembre");
+ }
 	
 }
